2c5d4373eff2d5bf0c864774ae93438ad6f88fda
Sean <thingshappen.xuras@gmail.com>
2019-08-24 23:03:09 +0100

    Added a new function that can concatenate text.

---
+++ InDoOut Core Plugins/Text/CombineTextFunction.cs
@@ -0,0 +1,42 @@
+﻿using InDoOut_Core.Entities.Functions;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace InDoOut_Core_Plugins.Text
+{
+    public class CombineTextFunction : Function
+    {
+        private static readonly int NUMBER_PROPERTIES_TO_CRETATE = 10;
+
+        private IOutput _output = null;
+        private IResult _result = null;
+        private List<IProperty> _properties = new List<IProperty>();
+
+        public override string Description => "Combines two or more text elements together.";
+
+        public override string Name => "Combine Text";
+
+        public override string Group => "Text";
+
+        public override string[] Keywords => new[] { "Concatenate", "Group", "Text", "String", "Merge" };
+
+        public CombineTextFunction()
+        {
+            _ = CreateInput("Combine");
+
+            _output = CreateOutput();
+            _result = AddResult(new Result("Combined text", "The combined text from the inputs", ""));
+
+            for (var count = 1; count <= NUMBER_PROPERTIES_TO_CRETATE; ++count)
+            {
+                _properties.Add(AddProperty(new Property<string>($"String {count}", $"String number {count} to combine.", false, "")));
+            }
+        }
+
+        protected override IOutput Started(IInput triggeredBy)
+        {
+            _result.RawValue = string.Concat(_properties.Select(property => property.RawValue));
+            return _output;
+        }
+    }
+}
