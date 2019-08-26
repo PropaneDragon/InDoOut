ac948fd2114d7387ba8bc132c068366c5cc8ba85
Sean <thingshappen.xuras@gmail.com>
2019-08-26 21:52:46 +0100

    Added a function to get the length of some text.

---
+++ InDoOut Core Plugins/Text/TextLengthFunction.cs
@@ -0,0 +1,39 @@
+﻿using InDoOut_Core.Entities.Functions;
+
+namespace InDoOut_Core_Plugins.Text
+{
+    public class TextLengthFunction : Function
+    {
+        private readonly IOutput _output = null;
+        private readonly IProperty _text = null;
+        private readonly IResult _length = null;
+
+        public override string Description => "Outputs the length of the given text (including spaces).";
+
+        public override string Name => "Count text";
+
+        public override string Group => "Text";
+
+        public override string[] Keywords => new[] { "length", "count", "size", "number", "text", "string", "characters" };
+
+        public TextLengthFunction()
+        {
+            _ = CreateInput("Count");
+            _output = CreateOutput();
+
+            _text = AddProperty(new Property<string>("Text", "The text to count", false, ""));
+            _length = AddResult(new Result("Text count", "The number of characters in the text (including spaces).", "0"));
+        }
+
+        protected override IOutput Started(IInput triggeredBy)
+        {
+            var text = _text.RawComputedValue;
+            if (text != null)
+            {
+                _ = _length.ValueFrom(text.Length);
+            }
+
+            return _output;
+        }
+    }
+}
