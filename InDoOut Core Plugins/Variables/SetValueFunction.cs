fd3aace44945303c00cf1c24499ddc717dfc090d
Sean <thingshappen.xuras@gmail.com>
2019-08-27 16:52:41 +0100

    Added a function to set any property to a custom value (such as resetting something to a default value when it already has inputs from results)

---
+++ InDoOut Core Plugins/Variables/SetValueFunction.cs
@@ -0,0 +1,34 @@
+﻿using InDoOut_Core.Entities.Functions;
+
+namespace InDoOut_Core_Plugins.Variables
+{
+    public class SetValueFunction : Function
+    {
+        IOutput _output = null;
+        IProperty<string> _valueToSet = null;
+        IResult _valueSet = null;
+
+        public override string Description => "Sets a value output from an input value.";
+
+        public override string Name => "Set value";
+
+        public override string Group => "Variables";
+
+        public override string[] Keywords => new[] { "values", "variables", "function", "output", "input", "property", "result", "set", "update" };
+
+        public SetValueFunction()
+        {
+            _ = CreateInput("Set");
+            _output = CreateOutput("Value is set");
+            _valueToSet = AddProperty(new Property<string>("Value to set", "The value to set."));
+            _valueSet = AddResult(new Result("Value out", "The value that has been set."));
+        }
+
+        protected override IOutput Started(IInput triggeredBy)
+        {
+            _valueSet.RawValue = _valueToSet.FullValue;
+
+            return _output;
+        }
+    }
+}
