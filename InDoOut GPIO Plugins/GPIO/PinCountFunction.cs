f40f4f89e3f56dcaad7ed190f91473a8b024dd02
Sean <thingshappen.xuras@gmail.com>
2019-08-29 21:37:45 +0100

    Experimental GPIO functions for RPi and other similar devices

---
+++ InDoOut GPIO Plugins/GPIO/PinCountFunction.cs
@@ -0,0 +1,44 @@
+﻿using InDoOut_Core.Entities.Functions;
+using InDoOut_Core.Threading.Safety;
+
+#if (!_WINDOWS)
+using System.Device.Gpio;
+#endif
+
+namespace InDoOut_GPIO_Plugins.GPIO
+{
+    public class PinCountFunction : Function
+    {
+        public IOutput _outputCounted = null;
+        public IResult _resultCount = null;
+
+        public override string Description => "Counts the number of available GPIO pins on the current board.";
+
+        public override string Name => "Count pins";
+
+        public override string Group => "GPIO";
+
+        public override string[] Keywords => new[] { "gpio", "pins", "raspberry", "pi", "IO", "system.device.gpio", "gpiocontroller" };
+
+        public PinCountFunction()
+        {
+            _ = CreateInput("Count pins");
+            _outputCounted = CreateOutput("Counted");
+            _resultCount = AddResult(new Result("Number of pins", "The number of pins returned by the board."));
+        }
+
+        protected override IOutput Started(IInput triggeredBy)
+        {
+#if (_WINDOWS)
+            _ = _resultCount.ValueFrom(0);
+#else
+            using (var controller = new GpioController())
+            {
+                _ = _resultCount.ValueFrom(TryGet.ValueOrDefault(() => controller.PinCount, 0));
+            }
+#endif
+
+            return _outputCounted;
+        }
+    }
+}
---
+++ InDoOut GPIO Plugins/PluginInfo.cs
@@ -0,0 +1,13 @@
+﻿using InDoOut_Plugins.Core;
+
+namespace InDoOut_Core_Plugins
+{
+    public class PluginInfo : Plugin
+    {
+        public override string Name => "GPIO Plugins";
+
+        public override string Description => "Provides GPIO interfacing on supported platforms.";
+
+        public override string Author => "ido";
+    }
+}
