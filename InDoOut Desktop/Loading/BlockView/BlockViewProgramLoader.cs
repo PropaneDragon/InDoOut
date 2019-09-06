d760835fcf41205e7a974bd5924fee79233ea81e
Sean <thingshappen.xuras@gmail.com>
2019-09-06 18:37:31 +0100

    Tidied up the BlockViewProgramLoader

--- InDoOut Desktop/Loading/BlockView/BlockViewProgramLoader.cs
+++ InDoOut Desktop/Loading/BlockView/BlockViewProgramLoader.cs
@@ -2,6 +2,7 @@
 using InDoOut_Core.Entities.Functions;
 using InDoOut_Core.Entities.Programs;
 using InDoOut_Desktop.UI.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
@@ -23,112 +24,131 @@ namespace InDoOut_Desktop.Loading.BlockView
             {
                 var functionToUIFunctionMap = new Dictionary<IFunction, IUIFunction>();
 
-                if (program.Metadata.ContainsKey("x") && program.Metadata.ContainsKey("y") && double.TryParse(program.Metadata["x"], out var screenLeft) && double.TryParse(program.Metadata["y"], out var screenTop) && _associatedBlockView is IScrollable scrollable)
+                if (ExtractLocation(program, out var location) && _associatedBlockView is IScrollable scrollable)
                 {
-                    scrollable.Offset = new Point(screenLeft, screenTop);
+                    scrollable.Offset = location;
                 }
 
                 foreach (var function in program.Functions)
                 {
-                    if (function.Metadata.ContainsKey("x") && function.Metadata.ContainsKey("y") && double.TryParse(function.Metadata["x"], out var xPosition) && double.TryParse(function.Metadata["y"], out var yPosition))
+                    var uiFunction = ExtractLocation(function, out var functionLocation) ? _associatedBlockView.Create(function, functionLocation) : _associatedBlockView.Create(function);
+                    if (uiFunction != null)
                     {
-                        var uiFunction = _associatedBlockView.Create(function, new Point(xPosition, yPosition));
-                        if (uiFunction != null)
-                        {
-                            functionToUIFunctionMap.Add(function, uiFunction);
-                        }
-                    }
-                    else
-                    {
-                        var uiFunction = _associatedBlockView.Create(function);
-                        if (uiFunction != null)
-                        {
-                            functionToUIFunctionMap.Add(function, uiFunction);
-                        }
+                        functionToUIFunctionMap.Add(function, uiFunction);
                     }
                 }
 
-                foreach (var function in program.Functions)
+                DisplayConnections(program, functionToUIFunctionMap);
+            }
+
+            return false;
+        }
+
+        private void DisplayConnections(IProgram program, Dictionary<IFunction, IUIFunction> functionToUIFunctionMap)
+        {
+            foreach (var function in program.Functions)
+            {
+                foreach (var output in function.Outputs)
                 {
-                    foreach (var output in function.Outputs)
+                    foreach (var input in output.Connections)
                     {
-                        foreach (var input in output.Connections)
+                        if (functionToUIFunctionMap.ContainsKey(function) && functionToUIFunctionMap.ContainsKey(input.Parent))
                         {
-                            if (functionToUIFunctionMap.ContainsKey(function) && functionToUIFunctionMap.ContainsKey(input.Parent))
+                            var startUiFunction = functionToUIFunctionMap[function];
+                            var endUiFunction = functionToUIFunctionMap[input.Parent];
+
+                            if (startUiFunction != null && endUiFunction != null)
                             {
-                                var startUiFunction = functionToUIFunctionMap[function];
-                                var endUiFunction = functionToUIFunctionMap[input.Parent];
+                                var uiOutput = startUiFunction.Outputs.FirstOrDefault(uiOutput => uiOutput.AssociatedOutput == output);
+                                var uiInput = endUiFunction.Inputs.FirstOrDefault(uiInput => uiInput.AssociatedInput == input);
 
-                                if (startUiFunction != null && endUiFunction != null)
+                                if (uiOutput != null && uiInput != null)
                                 {
-                                    var uiOutput = startUiFunction.Outputs.FirstOrDefault(uiOutput => uiOutput.AssociatedOutput == output);
-                                    var uiInput = endUiFunction.Inputs.FirstOrDefault(uiInput => uiInput.AssociatedInput == input);
-
-                                    if (uiOutput != null && uiInput != null)
-                                    {
-                                        var connection = _associatedBlockView.Create(uiOutput, uiInput);
-
-                                        if (ExtractMetadataValue(output, "x", out var outputX) && ExtractMetadataValue(output, "y", out var outputY) && ExtractMetadataValue(output, "w", out var outputW) && ExtractMetadataValue(output, "h", out var outputH) &&
-                                            ExtractMetadataValue(input, "x", out var inputX) && ExtractMetadataValue(input, "y", out var inputY) && ExtractMetadataValue(input, "w", out var inputW) && ExtractMetadataValue(input, "h", out var inputH))
-                                        {
-                                            var outputArea = new Rect(outputX, outputY, outputW, outputH);
-                                            var inputArea = new Rect(inputX, inputY, inputW, inputH);
-                                            var outputCentre = outputArea.TopLeft + ((outputArea.BottomRight - outputArea.TopLeft) / 2d);
-                                            var inputCentre = inputArea.TopLeft + ((inputArea.BottomRight - inputArea.TopLeft) / 2d);
-
-                                            connection.Start = _associatedBlockView.GetBestSide(outputArea, inputCentre);
-                                            connection.End = _associatedBlockView.GetBestSide(inputArea, outputCentre);
-                                        }
-                                    }
+                                    CreateConnection(uiOutput, uiInput, output, input);                                    
                                 }
                             }
                         }
                     }
+                }
 
-                    foreach (var result in function.Results)
+                foreach (var result in function.Results)
+                {
+                    foreach (var property in result.Connections)
                     {
-                        foreach (var property in result.Connections)
+                        if (functionToUIFunctionMap.ContainsKey(function) && functionToUIFunctionMap.ContainsKey(property.Parent))
                         {
-                            if (functionToUIFunctionMap.ContainsKey(function) && functionToUIFunctionMap.ContainsKey(property.Parent))
+                            var startUiFunction = functionToUIFunctionMap[function];
+                            var endUiFunction = functionToUIFunctionMap[property.Parent];
+
+                            if (startUiFunction != null && endUiFunction != null)
                             {
-                                var startUiFunction = functionToUIFunctionMap[function];
-                                var endUiFunction = functionToUIFunctionMap[property.Parent];
+                                var uiResult = startUiFunction.Results.FirstOrDefault(uiResult => uiResult.AssociatedResult == result);
+                                var uiProperty = endUiFunction.Properties.FirstOrDefault(uiProperty => uiProperty.AssociatedProperty == property);
 
-                                if (startUiFunction != null && endUiFunction != null)
+                                if (uiResult != null && uiProperty != null)
                                 {
-                                    var uiResult = startUiFunction.Results.FirstOrDefault(uiResult => uiResult.AssociatedResult == result);
-                                    var uiProperty = endUiFunction.Properties.FirstOrDefault(uiProperty => uiProperty.AssociatedProperty == property);
-
-                                    if (uiResult != null && uiProperty != null)
-                                    {
-                                        var connection = _associatedBlockView.Create(uiResult, uiProperty);
-
-                                        if (ExtractMetadataValue(result, "x", out var outputX) && ExtractMetadataValue(result, "y", out var outputY) && ExtractMetadataValue(result, "w", out var outputW) && ExtractMetadataValue(result, "h", out var outputH) &&
-                                            ExtractMetadataValue(property, "x", out var inputX) && ExtractMetadataValue(property, "y", out var inputY) && ExtractMetadataValue(property, "w", out var inputW) && ExtractMetadataValue(property, "h", out var inputH))
-                                        {
-                                            var outputArea = new Rect(outputX, outputY, outputW, outputH);
-                                            var inputArea = new Rect(inputX, inputY, inputW, inputH);
-                                            var outputCentre = outputArea.TopLeft + ((outputArea.BottomRight - outputArea.TopLeft) / 2d);
-                                            var inputCentre = inputArea.TopLeft + ((inputArea.BottomRight - inputArea.TopLeft) / 2d);
-
-                                            connection.Start = _associatedBlockView.GetBestSide(outputArea, inputCentre);
-                                            connection.End = _associatedBlockView.GetBestSide(inputArea, outputCentre);
-                                        }
-                                    }
+                                    CreateConnection(uiResult, uiProperty, result, property);
                                 }
                             }
                         }
                     }
                 }
             }
+        }
 
-            return false;
+        private void CreateConnection(IUIConnectionStart uiStart, IUIConnectionEnd uiEnd, IStored start, IStored end)
+        {
+            if (uiStart != null && uiEnd != null && start != null && end != null)
+            {
+                var connection = _associatedBlockView.Create(uiStart, uiEnd);
+
+                if (ExtractArea(start, out var startArea) && ExtractArea(end, out var endArea))
+                {
+                    var startCentre = startArea.TopLeft + ((startArea.BottomRight - startArea.TopLeft) / 2d);
+                    var endCentre = endArea.TopLeft + ((endArea.BottomRight - endArea.TopLeft) / 2d);
+
+                    connection.Start = _associatedBlockView.GetBestSide(startArea, endCentre);
+                    connection.End = _associatedBlockView.GetBestSide(endArea, startCentre);
+                }
+                else
+                {
+                    _associatedBlockView.Remove(connection);
+                }
+            }
         }
 
         public bool UnloadProgram(IProgram program)
         {
             if (program != null)
             {
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool ExtractLocation(IStored stored, out Point location)
+        {
+            location = new Point();
+
+            if (stored != null && ExtractMetadataValue(stored, "x", out var x) && ExtractMetadataValue(stored, "y", out var y))
+            {
+                location = new Point(x, y);
+
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool ExtractArea(IStored stored, out Rect area)
+        {
+            area = new Rect();
+
+            if (stored != null && ExtractLocation(stored, out var location) && ExtractMetadataValue(stored, "w", out var width) && ExtractMetadataValue(stored, "h", out var height))
+            {
+                area = new Rect(location, new Size(width, height));
+
                 return true;
             }
 
