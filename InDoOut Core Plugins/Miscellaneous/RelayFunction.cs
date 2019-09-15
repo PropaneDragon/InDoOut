310109e33b6230b794a2921fcdbe766f67156955
Sean <thingshappen.xuras@gmail.com>
2019-09-15 23:20:40 +0100

    Added a relay function to simply pass inputs into and trigger a single output. Tidies up wires.

---
+++ InDoOut Core Plugins/Miscellaneous/RelayFunction.cs
@@ -0,0 +1,29 @@
+﻿using InDoOut_Core.Entities.Functions;
+
+namespace InDoOut_Core_Plugins.Miscellaneous
+{
+    public class RelayFunction : Function
+    {
+        private IOutput _output;
+
+        public override string Description => "Relays an input straight to an output. Keeps cables tidy.";
+
+        public override string Name => "Relay";
+
+        public override string Group => "Miscellaneous";
+
+        public override string[] Keywords => new[] { "relay", "cable", "wire", "tidy", "container", "passthrough", "tie", "wrap" };
+
+        public RelayFunction()
+        {
+            _ = CreateInput();
+
+            _output = CreateOutput();
+        }
+
+        protected override IOutput Started(IInput triggeredBy)
+        {
+            return _output;
+        }
+    }
+}
