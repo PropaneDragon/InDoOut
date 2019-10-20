b3039105206543d2cc5d16e977845e73b201960f
Sean <thingshappen.xuras@gmail.com>
2019-10-20 17:11:24 +0100

    Fixed loggging tests after logs were changed around.

--- Tests/InDoOut Core Tests/LoggingTests.cs
+++ Tests/InDoOut Core Tests/LoggingTests.cs
@@ -18,7 +18,7 @@ namespace InDoOut_Core_Tests
             log.Error("This is an error");
 
             Assert.AreEqual(1, log.Logs.Count);
-            Assert.AreEqual("This is an error", log.Logs[0].Message);
+            Assert.AreEqual($"[{log.Logs[0].Time.ToString("yyyy-MM-dd HH:mm:ss")}][{log.Logs[0].CallingAssembly.GetName().Name}][!] This is an error", log.Logs[0].ToString());
             Assert.AreEqual(Assembly.GetExecutingAssembly(), log.Logs[0].CallingAssembly);
             Assert.AreEqual(LogMessage.LogLevel.Error, log.Logs[0].Level);
             Assert.IsTrue((DateTime.Now - log.Logs[0].Time).TotalSeconds < 1d);
@@ -26,27 +26,27 @@ namespace InDoOut_Core_Tests
             log.Warning("This is a warning");
 
             Assert.AreEqual(2, log.Logs.Count);
-            Assert.AreEqual("This is a warning", log.Logs[1].Message);
+            Assert.AreEqual($"[{log.Logs[1].Time.ToString("yyyy-MM-dd HH:mm:ss")}][{log.Logs[1].CallingAssembly.GetName().Name}][@] This is a warning", log.Logs[1].ToString());
             Assert.AreEqual(Assembly.GetExecutingAssembly(), log.Logs[1].CallingAssembly);
             Assert.AreEqual(LogMessage.LogLevel.Warning, log.Logs[1].Level);
 
-            Assert.AreEqual("This is an error", log.Logs[0].Message);
+            Assert.AreEqual($"[{log.Logs[0].Time.ToString("yyyy-MM-dd HH:mm:ss")}][{log.Logs[0].CallingAssembly.GetName().Name}][!] This is an error", log.Logs[0].ToString());
             Assert.AreEqual(Assembly.GetExecutingAssembly(), log.Logs[0].CallingAssembly);
             Assert.AreEqual(LogMessage.LogLevel.Error, log.Logs[0].Level);
 
             log.Info("This is a message");
 
             Assert.AreEqual(3, log.Logs.Count);
-            Assert.AreEqual("This is a message", log.Logs[2].Message);
+            Assert.AreEqual($"[{log.Logs[2].Time.ToString("yyyy-MM-dd HH:mm:ss")}][{log.Logs[2].CallingAssembly.GetName().Name}][i] This is a message", log.Logs[2].ToString());
             Assert.AreEqual(Assembly.GetExecutingAssembly(), log.Logs[2].CallingAssembly);
             Assert.AreEqual(LogMessage.LogLevel.Info, log.Logs[2].Level);
             Assert.IsTrue((DateTime.Now - log.Logs[2].Time).TotalSeconds < 1d);
 
-            Assert.AreEqual("This is a warning", log.Logs[1].Message);
+            Assert.AreEqual($"[{log.Logs[1].Time.ToString("yyyy-MM-dd HH:mm:ss")}][{log.Logs[1].CallingAssembly.GetName().Name}][@] This is a warning", log.Logs[1].ToString());
             Assert.AreEqual(Assembly.GetExecutingAssembly(), log.Logs[1].CallingAssembly);
             Assert.AreEqual(LogMessage.LogLevel.Warning, log.Logs[1].Level);
 
-            Assert.AreEqual("This is an error", log.Logs[0].Message);
+            Assert.AreEqual($"[{log.Logs[0].Time.ToString("yyyy-MM-dd HH:mm:ss")}][{log.Logs[0].CallingAssembly.GetName().Name}][!] This is an error", log.Logs[0].ToString());
             Assert.AreEqual(Assembly.GetExecutingAssembly(), log.Logs[0].CallingAssembly);
             Assert.AreEqual(LogMessage.LogLevel.Error, log.Logs[0].Level);
 
@@ -55,12 +55,12 @@ namespace InDoOut_Core_Tests
             log.Info("This is another message");
 
             Assert.AreEqual(2, log.Logs.Count);
-            Assert.AreEqual("This is another message", log.Logs[1].Message);
+            Assert.AreEqual($"[{log.Logs[1].Time.ToString("yyyy-MM-dd HH:mm:ss")}][{log.Logs[1].CallingAssembly.GetName().Name}][i] This is another message", log.Logs[1].ToString());
             Assert.AreEqual(Assembly.GetExecutingAssembly(), log.Logs[1].CallingAssembly);
             Assert.AreEqual(LogMessage.LogLevel.Info, log.Logs[1].Level);
             Assert.IsTrue((DateTime.Now - log.Logs[1].Time).TotalSeconds < 1d);
 
-            Assert.AreEqual("This is a message", log.Logs[0].Message);
+            Assert.AreEqual($"[{log.Logs[0].Time.ToString("yyyy-MM-dd HH:mm:ss")}][{log.Logs[0].CallingAssembly.GetName().Name}][i] This is a message", log.Logs[0].ToString());
             Assert.AreEqual(Assembly.GetExecutingAssembly(), log.Logs[0].CallingAssembly);
             Assert.AreEqual(LogMessage.LogLevel.Info, log.Logs[0].Level);
         }
