8080498b6624dcd44b5e4b0d3ef3b3bb1896e595
Sean <thingshappen.xuras@gmail.com>
2019-11-17 14:29:49 +0000

    Added a function to parse a JSON value that could be returned by the JSON path function.

---
+++ InDoOut Data Plugins/Json/GetValueFunction.cs
@@ -0,0 +1,46 @@
+﻿using InDoOut_Core.Entities.Functions;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace InDoOut_Data_Plugins.Json
+{
+    public class GetValueFunction : Function
+    {
+        private readonly IOutput _output, _error;
+        private readonly IProperty<string> _json;
+        private readonly IResult _value;
+
+        public override string Description => "Gets the value of a JSON object";
+
+        public override string Name => "Get JSON value";
+
+        public override string Group => "JSON";
+
+        public override string[] Keywords => new[] { "" };
+
+        public override IOutput TriggerOnFailure => _error;
+
+        public GetValueFunction()
+        {
+            _ = CreateInput();
+
+            _output = CreateOutput();
+            _error = CreateOutput("Error", OutputType.Negative);
+
+            _json = AddProperty(new Property<string>("JSON input", "The JSON object to get the value for."));
+
+            _value = AddResult(new Result("Value", "The value of the given JSON object"));
+        }
+
+        protected override IOutput Started(IInput triggeredBy)
+        {
+            var jsonValue = JToken.Parse(_json.FullValue);
+            if (jsonValue != null)
+            {
+                return _value.ValueFrom(jsonValue.Value<string>()) ? _output : _error;
+            }
+
+            return _error;
+        }
+    }
+}
