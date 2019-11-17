e15be10f2266678dcbffd2942dadf9501c60eae2
Sean <thingshappen.xuras@gmail.com>
2019-11-17 14:48:45 +0000

    Added functions to create and append to text files.

---
+++ InDoOut Core Plugins/Files/AppendFileFunction.cs
@@ -0,0 +1,38 @@
+﻿using InDoOut_Core.Entities.Functions;
+using System.IO;
+
+namespace InDoOut_Core_Plugins.Files
+{
+    public class AppendFileFunction : Function
+    {
+        private readonly IOutput _appended, _error;
+        private readonly IProperty<string> _path, _contents;
+
+        public override string Description => "Appends text to the end of a file at the specified location.";
+
+        public override string Name => "Append file";
+
+        public override string Group => "Files";
+
+        public override string[] Keywords => new[] { "text", "dir", "directory", "files", "txt", "add", "concatenate", "last" };
+
+        public override IOutput TriggerOnFailure => _error;
+
+        public AppendFileFunction()
+        {
+            _ = CreateInput("Append file");
+
+            _appended = CreateOutput("Appended", OutputType.Positive);
+            _error = CreateOutput("Error", OutputType.Negative);
+
+            _path = AddProperty(new Property<string>("Path", "The path of the file to append text to.", true));
+            _contents = AddProperty(new Property<string>("Contents", "The contents to append to the end of the file.", true));
+        }
+
+        protected override IOutput Started(IInput triggeredBy)
+        {
+            File.AppendAllText(_path.FullValue, _contents.FullValue);
+            return _appended;
+        }
+    }
+}
---
+++ InDoOut Core Plugins/Files/CreateFileFunction.cs
@@ -0,0 +1,38 @@
+﻿using InDoOut_Core.Entities.Functions;
+using System.IO;
+
+namespace InDoOut_Core_Plugins.Files
+{
+    public class CreateFileFunction : Function
+    {
+        private readonly IOutput _created, _error;
+        private readonly IProperty<string> _path, _contents;
+
+        public override string Description => "Creates a text file at the specified location.";
+
+        public override string Name => "Create a file";
+
+        public override string Group => "Files";
+
+        public override string[] Keywords => new[] { "text", "dir", "directory", "files", "txt" };
+
+        public override IOutput TriggerOnFailure => _error;
+
+        public CreateFileFunction()
+        {
+            _ = CreateInput("Create file");
+
+            _created = CreateOutput("Created", OutputType.Positive);
+            _error = CreateOutput("Error", OutputType.Negative);
+
+            _path = AddProperty(new Property<string>("Path", "The path (including file name and extension) to create the file at.", true));
+            _contents = AddProperty(new Property<string>("Contents", "The contents to put into the file when it is created.", true));
+        }
+
+        protected override IOutput Started(IInput triggeredBy)
+        {
+            File.WriteAllText(_path.FullValue, _contents.FullValue);
+            return _created;
+        }
+    }
+}
