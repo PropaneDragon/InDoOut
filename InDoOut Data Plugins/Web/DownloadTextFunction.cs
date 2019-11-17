6e3ce2364ae38be111612026aa4ef349834a08db
Sean <thingshappen.xuras@gmail.com>
2019-11-17 15:29:09 +0000

    Added a function to download text from any URL

---
+++ InDoOut Data Plugins/Web/DownloadTextFunction.cs
@@ -0,0 +1,40 @@
+﻿using InDoOut_Core.Entities.Functions;
+using System.Net;
+
+namespace InDoOut_Data_Plugins.Web
+{
+    public class DownloadTextFunction : Function
+    {
+        private readonly IOutput _downloaded, _error;
+        private readonly IProperty<string> _url;
+        private readonly IResult _text;
+
+        public override string Description => "Downloads text from a given URL";
+
+        public override string Name => "Download text";
+
+        public override string Group => "Web";
+
+        public override string[] Keywords => new[] { "download", "txt", "contents", "page", "webpage", "url", "api", "json", "xml", "html", "css" };
+
+        public override IOutput TriggerOnFailure => _error;
+
+        public DownloadTextFunction()
+        {
+            _ = CreateInput("Download");
+
+            _downloaded = CreateOutput("Downloaded", OutputType.Positive);
+            _error = CreateOutput("Error", OutputType.Negative);
+
+            _url = AddProperty(new Property<string>("URL", "The URL to download the text from.", true));
+
+            _text = AddResult(new Result("Downloaded text", "The download text from the given URL."));
+        }
+
+        protected override IOutput Started(IInput triggeredBy)
+        {
+            using var client = new WebClient();
+            return _text.ValueFrom(client.DownloadString(_url.FullValue)) ? _downloaded : _error;
+        }
+    }
+}
