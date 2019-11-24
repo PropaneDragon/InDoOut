be876ca4f9b4c501c1aaea9056ac694627fb24b8
Sean <thingshappen.xuras@gmail.com>
2019-11-24 23:07:45 +0000

    Added processor usage percentage and total memory functions, but disabled them for now as they don't work correctly.

---
+++ InDoOut Core Plugins/SystemInfo/GetProcessorUsagePercentageFunction.cs
@@ -0,0 +1,57 @@
+﻿using InDoOut_Core.Entities.Functions;
+using InDoOut_Core.Threading.Safety;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+
+namespace InDoOut_Core_Plugins.SystemInfo
+{
+    class GetProcessorUsagePercentageFunction : Function
+    {
+        readonly IOutput _output;
+        readonly IResult _cpuPercentage;
+
+        public override string Description => "Gets current processor usage as a percentage. This gives a very limited usage view in non-administrator mode.";
+
+        public override string Name => "Get current processor usage";
+
+        public override string Group => "System";
+
+        public override string[] Keywords => new string[] { "cpu", "computer", "time", "milliseconds", "ms" };
+
+        public override IOutput TriggerOnFailure => _output;
+
+        public GetProcessorUsagePercentageFunction()
+        {
+            _ = CreateInput("Get usage");
+
+            _output = CreateOutput();
+
+            _cpuPercentage = AddResult(new Result("Processor usage percentage", "The percentage of the processor currently being used", "0"));
+        }
+
+        protected override IOutput Started(IInput triggeredBy)
+        {
+            var stopwatch = new Stopwatch();
+            var allProcesses = System.Diagnostics.Process.GetProcesses();
+
+            stopwatch.Start();
+
+            var totalUsageBefore = allProcesses.Select(process => TryGet.ValueOrDefault(() => process.TotalProcessorTime.TotalMilliseconds, 0)).Sum();
+
+            Thread.Sleep(TimeSpan.FromMilliseconds(100));
+
+            var totalUsageAfter = allProcesses.Select(process => TryGet.ValueOrDefault(() => process.TotalProcessorTime.TotalMilliseconds, 10)).Sum();
+
+            stopwatch.Stop();
+
+            var totalUsageBetween = totalUsageAfter - totalUsageBefore;
+            var totalCpuUsage = Math.Clamp(totalUsageBetween / (Environment.ProcessorCount * stopwatch.ElapsedMilliseconds), 0d, 1d);
+
+            _ = _cpuPercentage.ValueFrom(totalCpuUsage * 100d);
+
+            return _output;
+        }
+    }
+}
---
+++ InDoOut Core Plugins/SystemInfo/GetTotalMemoryFunction.cs
@@ -0,0 +1,37 @@
+﻿using InDoOut_Core.Entities.Functions;
+using System;
+
+namespace InDoOut_Core_Plugins.SystemInfo
+{
+    /*public */class GetTotalMemoryFunction : Function
+    {
+        readonly IOutput _output;
+        readonly IResult _memoryBits;
+
+        public override string Description => "Gets total memory in the current machine.";
+
+        public override string Name => "Get total memory";
+
+        public override string Group => "System";
+
+        public override string[] Keywords => new string[] { "ram" };
+
+        public override IOutput TriggerOnFailure => _output;
+
+        public GetTotalMemoryFunction()
+        {
+            _ = CreateInput("Get memory");
+
+            _output = CreateOutput();
+
+            _memoryBits = AddResult(new Result("Total memory (bits)", "Total bits of memory present in the machine", "0"));
+        }
+
+        protected override IOutput Started(IInput triggeredBy)
+        {
+            _memoryBits.ValueFrom(Environment.WorkingSet);
+
+            return _output;
+        }
+    }
+}
