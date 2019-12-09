d7ff30ff750d5161c79fddf53bb348a6fb1a9fba
Sean <thingshappen.xuras@gmail.com>
2019-12-09 13:16:41 +0000

    Implemented actions and a basic resting action for the ScreenOverview panel.

---
+++ InDoOut Display/Actions/ScreenItemRestingAction.cs
@@ -0,0 +1,19 @@
+﻿using InDoOut_UI_Common.Actions;
+using System.Windows;
+
+namespace InDoOut_Display.Actions
+{
+    internal class ScreenItemRestingAction : Action
+    {
+        public ScreenItemRestingAction()
+        {
+        }
+
+        public override bool MouseLeftMove(Point mousePosition)
+        {
+
+
+            return false;
+        }
+    }
+}
--- InDoOut Display/UI/Controls/Screens/ScreenOverview.xaml.cs
+++ InDoOut Display/UI/Controls/Screens/ScreenOverview.xaml.cs
@@ -1,26 +1,87 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using InDoOut_Display.Actions;
+using InDoOut_UI_Common.Actions;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace InDoOut_Display.UI.Controls.Screens
 {
-    /// <summary>
-    /// Interaction logic for ScreenOverview.xaml
-    /// </summary>
     public partial class ScreenOverview : UserControl
     {
+        private readonly ActionHandler _actionHandler = new ActionHandler(new ScreenItemRestingAction());
+
         public ScreenOverview()
         {
             InitializeComponent();
+        }
+
+        private void Scroll_Content_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _ = _actionHandler?.MouseLeftDown(e.GetPosition(sender as IInputElement));
+
+            e.Handled = false;
+        }
+
+        private void Scroll_Content_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            _ = _actionHandler?.MouseLeftUp(e.GetPosition(sender as IInputElement));
+
+            e.Handled = false;
+        }
+
+        private void Scroll_Content_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                _ = _actionHandler?.MouseLeftMove(e.GetPosition(sender as IInputElement));
+            }
+#pragma warning disable IDE0045 // Convert to conditional expression
+            else if (e.RightButton == MouseButtonState.Pressed)
+#pragma warning restore IDE0045 // Convert to conditional expression
+            {
+                _ = _actionHandler?.MouseRightMove(e.GetPosition(sender as IInputElement));
+            }
+            else
+            {
+                _ = _actionHandler?.MouseNoMove(e.GetPosition(sender as IInputElement));
+            }
+
+            e.Handled = false;
+        }
+
+        private void Scroll_Content_PreviewMouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _ = _actionHandler?.MouseRightDown(e.GetPosition(sender as IInputElement));
+
+            e.Handled = false;
+        }
+
+        private void Scroll_Content_PreviewMouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            _ = _actionHandler?.MouseRightUp(e.GetPosition(sender as IInputElement));
+
+            e.Handled = false;
+        }
+
+        private void Scroll_Content_PreviewMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            _ = _actionHandler?.MouseDoubleClick(e.GetPosition(sender as IInputElement));
+
+            e.Handled = false;
+        }
+
+        private void Scroll_Content_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            _ = _actionHandler?.KeyDown(e.Key);
+
+            e.Handled = false;
+        }
+
+        private void Scroll_Content_PreviewKeyUp(object sender, KeyEventArgs e)
+        {
+            _ = _actionHandler?.KeyUp(e.Key);
+
+            e.Handled = false;
         }
     }
 }
