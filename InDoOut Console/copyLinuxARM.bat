8c3ab97d606eac45adddcc65e53a00dabf9c9380
Sean <thingshappen.xuras@gmail.com>
2020-01-15 20:26:18 +0000

    Using rem instead of # because I'm dumb

--- InDoOut Console/copyLinuxARM.bat
+++ InDoOut Console/copyLinuxARM.bat
@@ -1,3 +1,3 @@
-#scp -r bin/Publish/Linux/ARM/ pi@192.168.1.11:~/Downloads
-#scp -r bin/Publish/Linux/ARM/ pi@pi.hole:~/Downloads
+rem scp -r bin/Publish/Linux/ARM/ pi@192.168.1.11:~/Downloads
+rem scp -r bin/Publish/Linux/ARM/ pi@pi.hole:~/Downloads
 scp -r bin/Publish/Linux/ARM/ linaro@tinkerboard.lan:~/Downloads
