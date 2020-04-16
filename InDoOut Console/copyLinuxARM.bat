77fc1a9f8bfc96eafafb1ec092f6363f80c90fa1
Sean <thingshappen.xuras@gmail.com>
2020-04-16 22:52:18 +0100

    Added another line to the internal copy batch file that just copies programs.

--- InDoOut Console/copyLinuxARM.bat
+++ InDoOut Console/copyLinuxARM.bat
@@ -1,3 +1,4 @@
 rem scp -r bin/Publish/Linux/ARM/ pi@192.168.1.11:~/Downloads
 rem scp -r bin/Publish/Linux/ARM/ pi@pi.hole:~/Downloads
 scp -r bin/Publish/Linux/ARM/ linaro@tinkerboard.lan:~/Downloads
+rem scp -r bin/Publish/Linux/ARM/Programs linaro@tinkerboard.lan:~/Downloads/ARM
