e63adeeadc2c88feec1105d95a7df140647efd97
Sean <thingshappen.xuras@gmail.com>
2020-04-27 23:09:39 +0100

    Changed application icons to the colour versions of the logo. Added colour logos to the Logos.xml file, and implemented a feature in the BackgroundImage that allows background icons to be selected. Changed instances of the icon on the splash screens for both IDO Desktop and IDO Display to be colour.

--- InDoOut UI Common/Controls/Background/BackgroundImage.xaml.cs
+++ InDoOut UI Common/Controls/Background/BackgroundImage.xaml.cs
@@ -1,12 +1,56 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace InDoOut_UI_Common.Controls.Background
 {
     public partial class BackgroundImage : UserControl
     {
+        public enum ImageStyles { Light, Dark, Colour };
+
+        private ImageStyles _imageStyle = ImageStyles.Light;
+
+        public ImageStyles ImageStyle { get => _imageStyle; set => SetImageStyle(value); }
+
         public BackgroundImage()
         {
             InitializeComponent();
+
+            SetImageStyle(ImageStyle);
+        }
+
+        private void SetImageStyle(ImageStyles style)
+        {
+            _imageStyle = style;
+
+            SetBackgroundImage(style);
+        }
+
+        private void SetBackgroundImage(ImageStyles style)
+        {
+            var images = Grid_Images.Children;
+
+            foreach (UIElement image in images)
+            {
+                image.Visibility = Visibility.Collapsed;
+            }
+
+            var elementToShow = GetElementForStyle(style);
+            if (elementToShow != null)
+            {
+                elementToShow.Visibility = Visibility.Visible;
+            }
+        }
+
+        private UIElement GetElementForStyle(ImageStyles style)
+        {
+            return style switch
+            {
+                ImageStyles.Colour => Image_Colour,
+                ImageStyles.Dark => Image_Dark,
+                ImageStyles.Light => Image_Light,
+                _ => Image_Light
+            };
         }
     }
 }
