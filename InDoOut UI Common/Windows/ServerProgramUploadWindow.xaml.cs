761bdf5a1f47a7b01d0bb88d2f8ee733cbd09c66
Sean <thingshappen.xuras@gmail.com>
2021-06-05 21:03:06 +0100

    Made a start on a window to upload programs to the server. Just the basic UI for now. Nothing actually happens yet, but it needs thought as to whether it operates like the program selection dialog that is used to download programs, or whether it does everything internally.

---
+++ InDoOut UI Common/Windows/ServerProgramUploadWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using InDoOut_Executable_Core.Messaging;
+using InDoOut_Networking.Client.Commands;
+using InDoOut_UI_Common.SaveLoad;
+using System.Windows;
+
+namespace InDoOut_UI_Common.Windows
+{
+    public partial class ServerProgramUploadWindow : Window
+    {
+        public ServerProgramUploadWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Select_Click(object sender, RoutedEventArgs e)
+        {
+            var program = await CommonProgramSaveLoad.Instance.LoadProgramDialogAsync(this);
+            if (program != null)
+            {
+            }
+            else
+            {
+                UserMessageSystemHolder.Instance.CurrentUserMessageSystem.ShowError("Invalid program", "The selected program appears to be invalid and can't be uploaded.")
+            }
+        }
+
+        private void Button_Upload_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
