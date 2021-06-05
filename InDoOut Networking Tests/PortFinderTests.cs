d5ae1c47e2d4231687fd3fff586b1e57157cd9cc
Sean <thingshappen.xuras@gmail.com>
2021-06-05 17:20:22 +0100

    Added a class to find an open port to eventually bind to in the server. PortFinder can find any available port or a port between a set range. Created tests to ensure it works as expected.

---
+++ InDoOut Networking Tests/PortFinderTests.cs
@@ -0,0 +1,49 @@
+﻿using InDoOut_Networking.Shared;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+using System.Net.Sockets;
+
+namespace InDoOut_Networking_Tests
+{
+    [TestClass]
+    public class PortFinderTests
+    {
+        [TestMethod]
+        public void AnyOpenPort()
+        {
+            var openPort = PortFinder.Find();
+
+            Assert.AreNotEqual(-1, openPort);
+
+            var listener = new TcpListener(IPAddress.Loopback, openPort);
+            listener.Start();
+
+            Assert.AreEqual((listener.LocalEndpoint as IPEndPoint).Port, openPort);
+            Assert.AreNotEqual(openPort, PortFinder.Find());
+
+            var clashListener = new TcpListener(IPAddress.Loopback, openPort);
+
+            _ = Assert.ThrowsException<SocketException>(() => clashListener.Start());
+
+            listener.Stop();
+            clashListener.Stop();
+        }
+
+        [TestMethod]
+        public void AnyPortRange()
+        {
+            for (var count = 0; count < 20; ++count)
+            {
+                var openPort = PortFinder.Find();
+
+                Assert.AreNotEqual(-1, openPort);
+
+                var portEnd = openPort + 100;
+                var foundPort = PortFinder.Find(openPort, portEnd);
+
+                Assert.IsTrue(foundPort >= openPort);
+                Assert.IsTrue(foundPort < portEnd);
+            }
+        }
+    }
+}
---
+++ InDoOut Networking/Shared/PortFinder.cs
@@ -0,0 +1,70 @@
+﻿using InDoOut_Core.Logging;
+using System.Net;
+using System.Net.Sockets;
+
+namespace InDoOut_Networking.Shared
+{
+    public static class PortFinder
+    {
+        public static int Find()
+        {
+            var foundPort = -1;
+            var listener = new TcpListener(IPAddress.Loopback, 0);
+
+            try
+            {
+                Log.Instance.Info("Finding available port");
+
+                listener.Start();
+
+                foundPort = (listener.LocalEndpoint as IPEndPoint).Port;
+            }
+            catch
+            {
+                Log.Instance.Warning("Couldn't find any open ports.");
+            }
+            finally
+            {
+                listener.Stop();
+            }
+
+            return foundPort;
+        }
+
+        public static int Find(int start, int end)
+        {
+            var foundPort = false;
+
+            Log.Instance.Info("Scanning for open ports between ", start, " and ", end);
+
+            for (var currentPort = start; currentPort < end; ++currentPort)
+            {
+                var listener = new TcpListener(IPAddress.Loopback, currentPort);
+
+                try
+                {
+                    Log.Instance.Info("Checking port ", currentPort);
+
+                    listener.Start();
+
+                    foundPort = true;
+                }
+                catch
+                {
+                    Log.Instance.Warning("Attempted to start on port ", currentPort, " but failed. Trying next.");
+                }
+                finally
+                {
+                    listener.Stop();
+                }
+
+                if (foundPort)
+                {
+                    return currentPort;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
